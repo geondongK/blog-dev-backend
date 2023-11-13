e3128366a69ad6505a4871b3e5ca208d154ab6ed
GeonDong <ehd563@naver.com>
2023-11-13 17:45:59 +0900

    feat: 응답 API  - 처리여부, 상태, 메시지 데이터 API 추가

--- src/main/java/blogservices/blogdevbackend/domain/post/controller/PostController.java
+++ src/main/java/blogservices/blogdevbackend/domain/post/controller/PostController.java
@@ -1,26 +1,20 @@
 package blogservices.blogdevbackend.domain.post.controller;
 
-import blogservices.blogdevbackend.domain.post.domain.Post;
 import blogservices.blogdevbackend.domain.post.dto.PostRequestDto;
 import blogservices.blogdevbackend.domain.post.dto.PostResponseDto;
-import blogservices.blogdevbackend.domain.post.exception.Constants;
-//import blogservices.blogdevbackend.domain.post.exception.PostException;
 import blogservices.blogdevbackend.domain.post.service.PostService;
+import blogservices.blogdevbackend.global.common.response.CommonResponse;
+import blogservices.blogdevbackend.global.common.response.ListResponse;
+import blogservices.blogdevbackend.global.common.response.ResponseService;
+import blogservices.blogdevbackend.global.common.response.SingleResponse;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -29,51 +23,55 @@ import java.util.List;
 @Slf4j
 public class PostController {
 
-    private final PostService service;
+    private final PostService postService;
+    private final ResponseService responseService;
 
     /* 전체 게시물 */
     @GetMapping(value = "/post")
-    public List<PostResponseDto> getAllPost(int limit, int offset) {
-        return service.getAllPost(limit, offset);
+    public ListResponse<PostResponseDto> getAllPost(int limit, int offset) {
+        List<PostResponseDto> response = postService.getAllPost(limit, offset);
+        return responseService.getListResponse(response);
     }
 
     /* 해당 게시물 */
     @GetMapping(value = "/post/{postId}")
-    public List<PostResponseDto> getPost(@PathVariable long postId) {
-
-        log.info("postController::postId = {}", postId);
-
-        return service.getPost(postId);
+    public SingleResponse<List<PostResponseDto>> getPost(@PathVariable long postId) {
+        List<PostResponseDto> response = postService.getPost(postId);
+        return responseService.getSingResponse(response);
     }
 
     /* 게시물 작성 */
     @PostMapping(value = "/post")
-    public Long createPost(@RequestBody PostRequestDto request) {
-        return service.createPost(request);
+    public CommonResponse createPost(@RequestBody @Valid PostRequestDto request) {
+        Long response = postService.createPost(request);
+        return responseService.getSuccessResponse();
     }
 
     /* 게시물 수정 */
     @PutMapping(value = "/post/{postId}")
-    public Long updatePost(@PathVariable long postId, @RequestBody PostRequestDto request) {
-        return service.updatePost(postId, request);
+    public CommonResponse updatePost(@PathVariable long postId, @RequestBody PostRequestDto request) {
+        Long response = postService.updatePost(postId, request);
+        return responseService.getSuccessResponse();
     }
 
     /* 게시물 삭제 */
     @DeleteMapping(value = "/post/{postId}")
-    public void deletePost(@PathVariable long postId) {
-        service.deletePost(postId);
+    public CommonResponse deletePost(@PathVariable long postId) {
+        postService.deletePost(postId);
+        return responseService.getSuccessResponse();
     }
 
     /* 게시물 검색 */
     @GetMapping(value = "/post/search")
-    public List<PostResponseDto> searchPost(@RequestParam(value = "q", required = false) String q, int limit, int offset) {
-        return service.searchPost(q, limit, offset);
+    public SingleResponse<List<PostResponseDto>> searchPost(@RequestParam(value = "q", required = false) String q, int limit, int offset) {
+        List<PostResponseDto> response = postService.searchPost(q, limit, offset);
+        return responseService.getSingResponse(response);
     }
 
     /* 파일 업로드 */
     @PostMapping(value = "/upload")
     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
-        return ResponseEntity.ok(service.uploadFile(file));
+        return ResponseEntity.ok(postService.uploadFile(file));
     }
 
 }
