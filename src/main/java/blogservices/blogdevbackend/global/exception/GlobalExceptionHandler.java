03aa6d3668d6653f95fcbac92faae10fcd90b29c
GeonDong <ehd563@naver.com>
2023-11-13 18:45:47 +0900

    feat : RestControllerAdvice  - 유효성 검사 예외 처리 추가

--- src/main/java/blogservices/blogdevbackend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/blogservices/blogdevbackend/global/exception/GlobalExceptionHandler.java
@@ -2,27 +2,49 @@ package blogservices.blogdevbackend.global.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice // 전역 예외 처리
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {ConstraintViolationException.class, DataIntegrityViolationException.class})
-    protected ResponseEntity<GlobalErrorResponse> handleDataException() {
-        log.error("handleDataException throw Exception : {}", GlobalErrorCode.DUPLICATE_RESOURCE);
-        return GlobalErrorResponse.toResponseEntity(GlobalErrorCode.DUPLICATE_RESOURCE);
+    // hibernate 에러
+    protected ResponseEntity<Object> handleDataException() {
+        // log.error("handleCustomException throw CustomException : {}", GlobalExceptionResponseCode.VALIDATION_FAILED);
+        GlobalExceptionResponseCode errorCode = GlobalExceptionResponseCode.DUPLICATE_RESOURCE;
+        return ResponseEntity.status(errorCode.getHttpStatus()).body(errorCode);
+    }
+
+    @Override
+    /* 검증 에러
+
+      @ExceptionHandler에 이미 MethodArgumentNotValidException이 구현되어있기
+      때문에 GlobalExceptionHandler에서 동일한 예외 처리를 하게 되면, Ambiguous(모호성) 문제가 발생
+
+    */
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        GlobalExceptionResponseCode errorCode = GlobalExceptionResponseCode.VALIDATION_FAILED;
+        return ResponseEntity.status(errorCode.getHttpStatus()).body(new GlobalExceptionResponse(errorCode));
     }
 
     @ExceptionHandler(value = {GlobalException.class})
-    protected ResponseEntity<GlobalErrorResponse> handleGlobalException(GlobalException e) {
+    // 특정 에러
+    protected ResponseEntity<Object> handleGlobalException(GlobalException e) {
+        // GlobalExceptionResponseCode errorCode;
         log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
-        return GlobalErrorResponse.toResponseEntity(e.getErrorCode());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus()).body(new GlobalExceptionResponse(e.getErrorCode()));
     }
 
 
