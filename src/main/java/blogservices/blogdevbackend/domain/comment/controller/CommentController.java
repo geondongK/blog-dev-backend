3bce5c88b1b23e2f29c0a19fad1a277f24252a03
GeonDong <ehd563@naver.com>
2023-11-13 18:41:00 +0900

    feat: 응답 API  - 처리여부, 상태, 메시지 데이터 API 추가

--- src/main/java/blogservices/blogdevbackend/domain/comment/controller/CommentController.java
+++ src/main/java/blogservices/blogdevbackend/domain/comment/controller/CommentController.java
@@ -3,49 +3,56 @@ package blogservices.blogdevbackend.domain.comment.controller;
 import blogservices.blogdevbackend.domain.comment.dto.CommentRequestDto;
 import blogservices.blogdevbackend.domain.comment.dto.CommentResponseDto;
 import blogservices.blogdevbackend.domain.comment.service.CommentService;
-import blogservices.blogdevbackend.global.util.ResultDtoUtil;
+import blogservices.blogdevbackend.global.common.response.CommonResponse;
+import blogservices.blogdevbackend.global.common.response.ListResponse;
+import blogservices.blogdevbackend.global.common.response.ResponseService;
+import blogservices.blogdevbackend.global.common.response.SingleResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-// 보안으로 auth 임시로 설정
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class CommentController {
 
-    private final CommentService service;
+    private final CommentService commentService;
+    private final ResponseService responseService;
 
     /* 댓글 작성 */
     @PostMapping(value = "/comment")
-    public List<CommentResponseDto> createComment(@RequestBody CommentRequestDto request) {
-        return service.save(request);
+    public SingleResponse<List<CommentResponseDto>> createComment(@RequestBody @Valid CommentRequestDto request) {
+        List<CommentResponseDto> response = commentService.save(request);
+        return responseService.getSingResponse(response);
     }
 
     /* 해당 댓글 */
     @GetMapping(value = "/comment/{postId}")
-    public List<CommentResponseDto> getComment(@PathVariable long postId) {
-        return service.getComment(postId);
+    public ListResponse<CommentResponseDto> getComment(@PathVariable int postId) {
+        List<CommentResponseDto> response = commentService.getComment(postId);
+        return responseService.getListResponse(response);
     }
 
     /* 댓글 수정 */
     @PutMapping(value = "/comment/{commentId}")
-    public Long updateComment(@PathVariable long commentId, @RequestBody CommentRequestDto request) {
-        return service.update(commentId, request);
+    public CommonResponse updateComment(@PathVariable long commentId, @RequestBody CommentRequestDto request) {
+        Long response = commentService.update(commentId, request);
+        return responseService.getSuccessResponse();
     }
 
     /* 대댓글 존재 시 isDeleted 업데이트 */
     @PutMapping(value = "/comment/existComment/{commentId}")
-    public Long existsByComment(@PathVariable long commentId) {
-        return service.existsByComment(commentId);
+    public CommonResponse existsByComment(@PathVariable long commentId) {
+        Long response = commentService.existsByComment(commentId);
+        return responseService.getSuccessResponse();
     }
 
     /* 댓글 삭제 */
     @DeleteMapping(value = "/comment/{commentId}")
-    public void deleteComment(@PathVariable long commentId) {
-        service.deleteComment(commentId);
+    public CommonResponse deleteComment(@PathVariable long commentId) {
+        commentService.deleteComment(commentId);
+        return responseService.getSuccessResponse();
     }
 }
