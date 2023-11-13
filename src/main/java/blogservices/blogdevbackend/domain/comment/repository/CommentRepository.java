60fc4c19bff02719743bc725b0564c3bffd9926b
GeonDong <ehd563@naver.com>
2023-11-13 18:41:17 +0900

    chore: import  - import 파일명 변경

--- src/main/java/blogservices/blogdevbackend/domain/comment/repository/CommentRepository.java
+++ src/main/java/blogservices/blogdevbackend/domain/comment/repository/CommentRepository.java
@@ -2,11 +2,13 @@ package blogservices.blogdevbackend.domain.comment.repository;
 
 import blogservices.blogdevbackend.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findByPostId(Long postId);
+    List<Comment> findByPostId(int postId);
 
 }
