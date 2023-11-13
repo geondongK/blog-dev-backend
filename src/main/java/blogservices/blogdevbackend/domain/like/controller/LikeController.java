c47cb72eaf1927fcbbce15ad743e4ad2ee930518
GeonDong <ehd563@naver.com>
2023-11-13 18:49:02 +0900

    feat: 응답 API  - 처리여부, 상태, 메시지 데이터 API 추가

--- src/main/java/blogservices/blogdevbackend/domain/like/controller/LikeController.java
+++ src/main/java/blogservices/blogdevbackend/domain/like/controller/LikeController.java
@@ -3,6 +3,9 @@ package blogservices.blogdevbackend.domain.like.controller;
 import blogservices.blogdevbackend.domain.like.dto.LikeRequestDto;
 import blogservices.blogdevbackend.domain.like.dto.LikeResponseDto;
 import blogservices.blogdevbackend.domain.like.service.LikeService;
+import blogservices.blogdevbackend.global.common.response.CommonResponse;
+import blogservices.blogdevbackend.global.common.response.ResponseService;
+import blogservices.blogdevbackend.global.common.response.SingleResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -10,29 +13,32 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-// 보안으로 auth 임시로 설정
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @Slf4j
 public class LikeController {
 
-    private final LikeService service;
+    private final LikeService likeService;
+    private final ResponseService responseService;
 
     /* 좋아요 조회 */
     @GetMapping(value = "/like/{postId}")
-    public List<LikeResponseDto> getLike(@PathVariable int postId) {
-        return service.getLike(postId);
+    public SingleResponse<List<LikeResponseDto>> getLike(@PathVariable int postId) {
+        List<LikeResponseDto> response = likeService.getLike(postId);
+        return responseService.getSingResponse(response);
     }
 
     /* 좋아요 추가 */
     @PostMapping(value = "/like")
-    public List<LikeResponseDto> addLike(@RequestBody LikeRequestDto request) {
-        return service.addLike(request);
+    public SingleResponse<List<LikeResponseDto>> addLike(@RequestBody LikeRequestDto request) {
+        List<LikeResponseDto> response = likeService.addLike(request);
+        return responseService.getSingResponse(response);
     }
 
     /* 좋아요 삭제 */
     @DeleteMapping(value = "/like/{commentId}/{userId}")
-    public void deleteLike(@PathVariable int commentId, @PathVariable long userId) {
-        service.deleteLike(commentId, userId);
+    public CommonResponse deleteLike(@PathVariable int commentId, @PathVariable long userId) {
+        likeService.deleteLike(commentId, userId);
+        return responseService.getSuccessResponse();
     }
 }
