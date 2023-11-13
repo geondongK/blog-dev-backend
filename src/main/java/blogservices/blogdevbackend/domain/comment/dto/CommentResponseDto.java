dae1598238771aaedb4bb4805c425c68464028c2
GeonDong <ehd563@naver.com>
2023-11-13 18:42:56 +0900

    feat : 데이트타임  - createDate, updateDate JsonFormat 추가

--- src/main/java/blogservices/blogdevbackend/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/blogservices/blogdevbackend/domain/comment/dto/CommentResponseDto.java
@@ -1,6 +1,7 @@
 package blogservices.blogdevbackend.domain.comment.dto;
 
 import blogservices.blogdevbackend.domain.comment.domain.Comment;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -8,7 +9,7 @@ import java.time.LocalDateTime;
 @Getter
 public class CommentResponseDto {
     private Long commentId; // PK
-    private Long postId; // 게시물 PK
+    private Integer postId; // 게시물 PK
     private Long writerId; // 작성자 PK
     private Long parentId; // 부모 댓글 PK
     private Integer commentGroup; // 부모 댓글 PK
@@ -16,7 +17,9 @@ public class CommentResponseDto {
     private String description; // 내용
     private String writer; // 작성자
     private Boolean isDeleted; // 삭제 여부
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createDate; // 생성일
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateDate; // 수정일
 
     public CommentResponseDto(Comment entity) {
