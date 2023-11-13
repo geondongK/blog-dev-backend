82bedc7ba06822926c39c17820c8d47842a1b06d
GeonDong <ehd563@naver.com>
2023-11-13 17:52:43 +0900

    chore: import  - import 파일명 변경

--- src/main/java/blogservices/blogdevbackend/domain/post/repository/PostRepository.java
+++ src/main/java/blogservices/blogdevbackend/domain/post/repository/PostRepository.java
@@ -1,14 +1,12 @@
 package blogservices.blogdevbackend.domain.post.repository;
 
 import blogservices.blogdevbackend.domain.post.domain.Post;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
