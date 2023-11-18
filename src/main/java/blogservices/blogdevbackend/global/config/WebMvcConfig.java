5c1482c744a237557d125fd2e05170f99237d627
GeonDong <ehd563@naver.com>
2023-11-19 01:56:59 +0900

    Feat: cors config  - clientUri 암호화 추가  - 쿠키 허용 추가

--- src/main/java/blogservices/blogdevbackend/global/config/WebMvcConfig.java
+++ src/main/java/blogservices/blogdevbackend/global/config/WebMvcConfig.java
@@ -1,19 +1,25 @@
 package blogservices.blogdevbackend.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/* cors */
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Value("${spring.url.client-url}")
+    private String clientUri;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+                .allowedOrigins(clientUri)
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true); // 쿠키 허용
     }
 }
 
