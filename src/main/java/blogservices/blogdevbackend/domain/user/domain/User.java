983455ac85c7bb299691910000dfab09c9cb4312
GeonDong <ehd563@naver.com>
2023-11-19 02:31:33 +0900

    Feat: 유저 entity  - 컬럼 타입 변경  - 카카오 로그인 provider 추가

--- src/main/java/blogservices/blogdevbackend/domain/user/domain/User.java
+++ src/main/java/blogservices/blogdevbackend/domain/user/domain/User.java
@@ -1,5 +1,6 @@
 package blogservices.blogdevbackend.domain.user.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,88 +10,100 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+// 파일명 entity 이름 동일해야함, 아닐 시 지정
 @Entity
-@Table(name = "users") // 이름 변경해야함
+@Table(name = "user")
 public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Long userId; // PK
 
-//    private String userType;
-//
-//    private Long userTypeId;
+    @Column(length = 50)
+    private String email; // 이메일
 
-    private String email;
+    @Column(length = 100)
+    private String password; // 비밀번호
 
-    private String password;
+    @Column(length = 8)
+    private String name; // 유저 이름
 
-    private String name;
+    @Column(length = 10)
+    private String provider; // OAuth2 공급자
 
+    private Long providerId; // OAuth2 공급자 아이디
+
+    @Column(length = 10)
     @Enumerated(EnumType.STRING)
-    private Role role;
+    private Role role; // 유저 역활
 
-    //private LocalDateTime userCreateDate = LocalDateTime.now();
+    private LocalDateTime userCreateDate = LocalDateTime.now(); // 생성일
 
     @Builder
-    public User(Long userId, String email, String password, String name, Role role) {
+    public User(Long userId, String email, String password, String name, String provider, Long providerId, Role role) {
         this.userId = userId;
         this.email = email;
         this.password = password;
         this.name = name;
+        this.provider = provider;
+        this.providerId = providerId;
         this.role = role;
     }
 
     //부여된 권한 모음
     @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     // 사용자 패스워드 반환
     @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // read 못하게 설정
     public String getPassword() {
         return password;
     }
 
     // 사용자 id 반환
-//    @Override
-//    public String getUsername() {
-//        return email;
-//    }
     @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     public String getUsername() {
         return email;
     }
 
     // 계정 만료 여부
     @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     public boolean isAccountNonExpired() {
         return true;
     }
 
     // 계정 잠금 여부
     @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     public boolean isAccountNonLocked() {
         return true;
     }
 
     // 패스워드 만료 여부
     @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
     // 계정 사용 가능 여부
     @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     public boolean isEnabled() {
         return true;
     }
+
+
 }
