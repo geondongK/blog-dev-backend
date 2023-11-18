c8999dba105796df6ccd5d6c9893c7f779e25d77
GeonDong <ehd563@naver.com>
2023-11-19 02:07:07 +0900

    Feat: 응답 API  - 처리여부, 상태, 메시지 데이터 API 추가

--- src/main/java/blogservices/blogdevbackend/domain/user/controller/v1/AuthController.java
+++ src/main/java/blogservices/blogdevbackend/domain/user/controller/v1/AuthController.java
@@ -1,46 +1,51 @@
 package blogservices.blogdevbackend.domain.user.controller.v1;
 
 import blogservices.blogdevbackend.domain.user.dto.auth.LoginRequestDto;
+import blogservices.blogdevbackend.domain.user.dto.auth.LoginResponseDto;
 import blogservices.blogdevbackend.domain.user.dto.auth.SignupRequestDto;
 import blogservices.blogdevbackend.domain.user.dto.token.TokenDto;
 import blogservices.blogdevbackend.domain.user.dto.token.TokenRequestDto;
-import blogservices.blogdevbackend.domain.user.dto.user.UserResponseDto;
-import blogservices.blogdevbackend.domain.user.service.authService.AuthService;
+import blogservices.blogdevbackend.domain.user.service.auth.AuthService;
 
+import blogservices.blogdevbackend.global.common.response.CommonResponse;
+import blogservices.blogdevbackend.global.common.response.ResponseService;
+import blogservices.blogdevbackend.global.common.response.SingleResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
 
-    private final AuthService service;
+    private final AuthService authService;
+    private final ResponseService responseService;
 
     /* 회원가입 */
     @PostMapping("/signup")
-    public ResponseEntity<Long> signup(@RequestBody SignupRequestDto request) {
-
-
-        return ResponseEntity.ok(service.signup(request));
+    public CommonResponse signup(@RequestBody @Valid SignupRequestDto request) {
+        authService.signup(request);
+        return responseService.getSuccessResponse();
     }
 
     /* 로그인 */
     @PostMapping("/login")
-    public ResponseEntity<List<UserResponseDto>> login(@RequestBody LoginRequestDto request) {
-        return ResponseEntity.ok(service.login(request));
+    public SingleResponse<LoginResponseDto> login(@RequestBody @Valid LoginRequestDto request) {
+        LoginResponseDto response = authService.login(request);
+        return responseService.getSingResponse(response);
     }
 
     /* 토큰 재발급 */
     @PostMapping("/reissue")
     public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto request) {
-        return ResponseEntity.ok(service.reissue(request));
+        TokenDto response = authService.reissue(request);
+        return ResponseEntity.ok(response);
     }
-
-
 }
 
 
