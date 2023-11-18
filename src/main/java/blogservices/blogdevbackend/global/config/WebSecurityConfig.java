b53a2ff19a4da3e0cc5ceea66c4fecb6062793f3
GeonDong <ehd563@naver.com>
2023-11-19 01:55:38 +0900

    Feat: 엔드포인트  - 카카오 로그인 엔트포인트 추가

--- src/main/java/blogservices/blogdevbackend/global/config/WebSecurityConfig.java
+++ src/main/java/blogservices/blogdevbackend/global/config/WebSecurityConfig.java
@@ -1,16 +1,20 @@
 package blogservices.blogdevbackend.global.config;
 
-import blogservices.blogdevbackend.global.auth.*;
+import blogservices.blogdevbackend.global.jwt.exception.JwtAccessDeniedHandler;
+import blogservices.blogdevbackend.global.jwt.exception.JwtAuthEntryPoint;
+import blogservices.blogdevbackend.global.jwt.filter.JwtAuthFilter;
+import blogservices.blogdevbackend.global.jwt.provider.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 
 @Configuration
@@ -21,46 +25,49 @@ public class WebSecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
     private final JwtAuthEntryPoint jwtAuthEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final CorsFilter corsFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                .antMatchers("/h2-console/**", "/favicon.ico");
-    }
-
+    // 요청이 들어오면 Spring Security 확인.
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                // Basic 인증, Bearer token 인증방법을 사용하기 때문에 비활성화
+                .httpBasic().disable()
+
+                // 시큐리티는 기본적으로 세션을 사용
+                // 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                //.and()
+                //.logout().deleteCookies(CookieAuth)
+                .and()
+                .authorizeHttpRequests()
+                //.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                // 보호되지 않은 엔드포인트
+                .antMatchers("/api/v1/auth/**", "/api/v1/oauth/kakao/callback/**", "/api/v1/oauth/kakao/**").permitAll()
+                .antMatchers(HttpMethod.GET,
+                        "/api/v1/post/**", "/api/v1/comment/**", "/api/v1/like/**"
+                ).permitAll()
+                // 그 이외에는 인증이 필요
+                .anyRequest().authenticated()
+
+                // exception handling 클래스를 추가
+                .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthEntryPoint)
                 .accessDeniedHandler(jwtAccessDeniedHandler)
 
                 .and()
-                .headers()
-                .frameOptions()
-                .sameOrigin()
-
-                // CSRF 비활성화한다면 세션에 상태가 없어야 한다.
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-                // 시큐리티는 기본적으로 세션을 사용
-                // 여기서는 세션을 사용하지 않기 때문에 세션 설정을 Stateless 로 설정
-                .and()
-                .authorizeHttpRequests()
-                //.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .antMatchers("/api/v1/auth/**").permitAll()
-                .anyRequest().authenticated()
-
-                .and()
-                .apply(new JwtSecurityConfig(jwtTokenProvider));
+                .addFilter(corsFilter)
+                .addFilterBefore(new JwtAuthFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        //.apply(new JwtSecurityConfig(jwtTokenProvider));
 
         return http.build();
     }
