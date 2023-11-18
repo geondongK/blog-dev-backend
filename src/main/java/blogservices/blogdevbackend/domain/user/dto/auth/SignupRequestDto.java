bae8a82789f2fe49aac417b4c99c0bc1136fc89b
GeonDong <ehd563@naver.com>
2023-11-19 02:32:08 +0900

    Feat: 회원가입 API  - 회원가입 API 개발

--- src/main/java/blogservices/blogdevbackend/domain/user/dto/auth/SignupRequestDto.java
+++ src/main/java/blogservices/blogdevbackend/domain/user/dto/auth/SignupRequestDto.java
@@ -7,29 +7,46 @@ import lombok.*;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignupRequestDto {
+    @Email // null 과 "" 과 " " 모두 허용하지 않는다.
     private String email;
-    private String password;
-    private String name;
 
+    private String password;
+    @Size(min = 2, max = 6)
+    @NotBlank
+    private String name;
+    private String passwordConfirm;
+    private String provider;
+    private Long providerId;
+
+    // 일반 회원가입
     public User toEntity(PasswordEncoder passwordEncoder) {
         return User.builder()
-                .email(email)
-                .password(passwordEncoder.encode(password))
-                .name(name)
-                .role(Role.ROLE_USER)
+                .email(email) // 이메일
+                .password(passwordEncoder.encode(password)) // 비밀번호
+                .name(name) // 이름
+                .role(Role.ROLE_USER) // 유저 역활
                 .build();
     }
 
+    // OAuth2 회원가입
     public User toEntity() {
         return User.builder()
                 .email(email)
                 .name(name)
+                .provider(provider)
+                .providerId(providerId)
+                .role(Role.ROLE_USER) // 유저 역활
                 .build();
     }
+
 
 }
