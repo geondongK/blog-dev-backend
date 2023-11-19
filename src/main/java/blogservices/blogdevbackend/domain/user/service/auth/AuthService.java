c6debf926e5aebd37f00a16ef1dca452cabd7d26
GeonDong <ehd563@naver.com>
2023-11-20 02:03:10 +0900

    Feat : 로그인 API  - 로그인 서비스 개발

--- src/main/java/blogservices/blogdevbackend/domain/user/service/auth/AuthService.java
+++ src/main/java/blogservices/blogdevbackend/domain/user/service/auth/AuthService.java
@@ -1,24 +1,17 @@
-package blogservices.blogdevbackend.domain.user.service.authService;
+package blogservices.blogdevbackend.domain.user.service.auth;
 
 import blogservices.blogdevbackend.domain.user.dto.auth.LoginRequestDto;
+import blogservices.blogdevbackend.domain.user.dto.auth.LoginResponseDto;
 import blogservices.blogdevbackend.domain.user.dto.auth.SignupRequestDto;
 import blogservices.blogdevbackend.domain.user.dto.token.TokenDto;
 import blogservices.blogdevbackend.domain.user.dto.token.TokenRequestDto;
-import blogservices.blogdevbackend.domain.user.dto.user.UserResponseDto;
-
-import java.util.List;
 
 public interface AuthService {
 
     Long signup(SignupRequestDto request);
 
-    List<UserResponseDto> login(LoginRequestDto request);
+    LoginResponseDto login(LoginRequestDto request);
 
     TokenDto reissue(TokenRequestDto request);
-
-    //    List<AuthRequestDTO> login(String email);
-
-//    Long signup(loginRequestDto authRequestDTO);
-
 
 }
