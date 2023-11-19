508b0553c2ffe2d5f16911dd3086a59621dd3871
GeonDong <ehd563@naver.com>
2023-11-20 02:06:20 +0900

    Feat : AWS S3  - 파일 업로드명 설정 메소드 개발

--- src/main/java/blogservices/blogdevbackend/global/common/S3Utils.java
+++ src/main/java/blogservices/blogdevbackend/global/common/S3Utils.java
@@ -1,6 +1,11 @@
 package blogservices.blogdevbackend.global.common;
 
+import blogservices.blogdevbackend.global.exception.GlobalException;
+import blogservices.blogdevbackend.global.exception.GlobalExceptionResponseCode;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 // S3 업로드 설정
@@ -9,9 +14,31 @@ public class S3Utils {
     private static final String TIME_SEPARATOR = "_";
     // private static final String CATEGORY_PREFIX = "/";
 
-    public static String buildFileName(String originalFileName) {
+    public static String buildFileName(MultipartFile originalFileName) {
+
+        String fileName = originalFileName.getOriginalFilename();
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(originalFileName.getSize());
+        metadata.setContentType(originalFileName.getContentType());
+
+        String fileType = originalFileName.getContentType();
+        String originalFileExtension;
+
+        if (ObjectUtils.isEmpty(fileType)) {
+            return null;
+        } else if (fileType.contains("image/jpeg")) {
+            originalFileExtension = ".jpg";
+        } else if (fileType.contains("image/gif")) {
+            originalFileExtension = ".png";
+        } else if (fileType.contains("image/png"))
+            originalFileExtension = ".gif";
+        else {
+            throw new GlobalException(GlobalExceptionResponseCode.FILE_UPLOAD_TYPE_ERROR);
+        }
+
         String now = String.valueOf(System.currentTimeMillis());
 
-        return now + FILE_EXTENSION_SEPARATOR + originalFileName;
+        return now + FILE_EXTENSION_SEPARATOR + fileName + originalFileExtension;
     }
 }
